9ac6d0e346d72bf89e44ba1ba165bf717028d59c
VPA273 <vpa273@gmail.com>
2021-03-13 21:12:21 +0300

    refactor: apply prettier

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let digit, result = 0
+    while( n ) {
+      digit = n % 10
+      result = (result * 10) + digit
+      n = n/10|0
+    }  
+  return ( Math.abs(result) )
 }
